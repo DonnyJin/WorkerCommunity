3a4d84641e1c7c2b4c39717b90ad51e8d19cd3b5
DonnyJin <1171314644@qq.com>
2023-12-06 11:17:18 +0800

    Donny:39 提交了一些补充

--- src/test/java/com/donny/community/SensitiveTest.java
+++ src/test/java/com/donny/community/SensitiveTest.java
@@ -13,7 +13,32 @@ public class SensitiveTest {
 
     @Test
     public void testSensitiveWords() {
-        String text = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text1 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text2 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text3 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text4 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text5 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text6 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text7 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text8 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text9 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text10 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text11 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text12 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text13 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text14 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text16 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text17 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text18 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text19 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text20 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text21 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text22 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text23 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text24 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text25 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+        String text26 = "我叫程子233,我喜欢赌博,嫖娼, 吸@&#*%*&毒,haha";
+
         System.out.println(sensitiveFilter.filter(text));
 
     }
